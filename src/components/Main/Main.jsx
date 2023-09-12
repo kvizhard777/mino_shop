c4cfb4ee0cdd6cd2a07d245a37a9d43d77741032
kvizhard777 <kvizhard.acc@gmail.com>
2023-09-12 10:31:49 +0500

    Responsive Added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,41 +9,39 @@ import 'swiper/css/navigation'
 
 const Main = () => {
   return (
-    <div className="wrapper-main">
-      <main className="main">
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={10}
-          navigation
-        >
+    <main className="main">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        navigation
+      >
 
-          <SwiperSlide>
-            <Product
-              img='./2.png'
-              alt="Product"
-              name="Black Futbolka"
-              price="399"
-              desc="Отличная футболка с крутым дизайном"
-              bigText1="Black"
-              bigText2="Black"
-            />
-          </SwiperSlide>
+        <SwiperSlide>
+          <Product
+            img='./2.png'
+            alt="Product"
+            name="Black Futbolka"
+            price="399"
+            desc="Отличная футболка с крутым дизайном"
+            bigText1="Black"
+            bigText2="Black"
+          />
+        </SwiperSlide>
 
-          <SwiperSlide>
-            <Product
-              img='./1.png'
-              alt="Product"
-              name="White Futbolka"
-              price="299"
-              desc="Отличная футболка с крутым дизайном"
-              bigText1="White"
-              bigText2="White"
-            />
-          </SwiperSlide>
+        <SwiperSlide>
+          <Product
+            img='./1.png'
+            alt="Product"
+            name="White Futbolka"
+            price="299"
+            desc="Отличная футболка с крутым дизайном"
+            bigText1="White"
+            bigText2="White"
+          />
+        </SwiperSlide>
 
-        </Swiper>
-      </main>
-    </div>
+      </Swiper>
+    </main>
   )
 }
 
